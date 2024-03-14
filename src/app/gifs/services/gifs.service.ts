8d47b724309d70ee53713cef0983ac5e587b0f10
Angel Morales Chocue <angelmrles7@gmail.com>
2024-03-14 13:39:19 -0500

    feat: Implement HTTP API call using HttpClientModule

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   private _tagsHistory: string[] = [];
-  constructor() {}
+  private apiKey: string = 'uhgyMwPq8nwy8jSv68KY3sB08ue5TIuC&q';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -25,5 +29,14 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+    // Observable
+    this.http.get(`${this.serviceUrl}/search${params}`).subscribe((res) => {
+      console.log(res);
+    });
   }
 }
